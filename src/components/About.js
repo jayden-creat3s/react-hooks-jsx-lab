761daa039661812a080292c8c7332383e2888c7f
jayden-creat3 <jaydenkamau508@gmail.com>
2025-07-24 16:30:03 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { image } from "../data/data";
+import { name, city, image } from "../data/data";
+
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I love building React applications and learning new technologies!</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
